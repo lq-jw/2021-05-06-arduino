ddb96505b9ad5c55ea994c70d83a8a25bc26c0c1
lq-jw <83740897+lq-jw@users.noreply.github.com>
2021-05-06 11:27:35 +0800

    Add files via upload

---
+++ wK11_LED.ino
@@ -0,0 +1,26 @@
+const int buttonPin = 2;
+const int ledPin = 13;
+
+int buttonState = 0;
+void setup() {
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+  pinMode(ledPin,OUTPUT);
+  pinMode(buttonPin,INPUT_PULLUP);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  buttonState = digitalRead(buttonPin);
+  Serial.print("THe button State = ");
+  Serial.println(buttonState);
+  delay(1);
+  if(buttonState == HIGH ){
+    digitalWrite(ledPin,LOW);
+    
+  }
+  else {
+    digitalWrite(ledPin,HIGH);
+    
+  }
+}
